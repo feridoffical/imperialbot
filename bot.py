2016cfe9c4f64c6240411fbd5603e2ba2102535f
feridoffical <110368464+feridoffical@users.noreply.github.com>
2022-08-01 20:46:29 +0400

    bot.py

---
+++ bot.py
@@ -0,0 +1,42 @@
+import os, logging, asyncio
+from telethon import Button
+from telethon import TelegramClient, events
+from telethon.tl.types import ChannelParticipantAdmin
+from telethon.tl.types import ChannelParticipantCreator
+from telethon.tl.functions.channels import GetParticipantRequest
+from telethon.errors import UserNotParticipantError
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(name)s - [%(levelname)s] - %(message)s'
+)
+LOGGER = logging.getLogger(__name__)
+
+api_id = int(os.environ.get("APP_ID"))
+api_hash = os.environ.get("API_HASH")
+bot_token = os.environ.get("TOKEN")
+client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
+spam_chats = []
+
+@client.on(events.NewMessage(pattern="^/start$"))
+async def start(event):
+  await event.reply(
+"""ʜᴏɪ,
+ɪ'ᴍ ʜᴇʀᴇ ᴛᴏ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ TAGALL ʏᴏᴜʀ ɢʀᴏᴜᴘꜱ ᴀɴᴅ ɪ ᴍ ᴠᴇʀʏ ᴘᴏᴡᴇʀꜰᴜʟʟ ʙᴏᴛ! 
+*SALAM ,*
+┏━━━━━━━━━━━━━━━━
+┣ ₪ *ADD ME YOUR GROUP* `
+┣ ₪ IAM POWER FULL TAGGER BOT
+┗━━━━━━━━━━━━━━━━━
+ 
+  ʜɪᴛ /help **FOR MORE**
+ [❤](https://telegra.ph/file/2fa3a833f3ccc1d98dba1.jpg),
+""",
+    link_preview=False,
+    buttons=(
+       [
+        Button.url(' support', 'https://t.me/chucky_support'),
+        Button.url('creater', 'https://t.me/kittu_the_criminall')
+    ],
+    )
+  )
