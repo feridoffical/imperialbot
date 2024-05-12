0ed76744f872108de98605a617cb4b436b7d4909
feridoffical <110368464+feridoffical@users.noreply.github.com>
2024-05-12 13:35:48 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,14 +21,13 @@ spam_chats = []
 @client.on(events.NewMessage(pattern="^/start$"))
 async def start(event):
   await event.reply(
-"""Salam,mən qruplarınızdakı istifadəçiləri tağ etmək üçün köməkçi botam.Məni qrupunuza əlavə edərək insanları rahatlıqla çağıra bilərsiz
-  /help **kömək üçün**[❤](https://telegra.ph/file/2fa3a833f3ccc1d98dba1.jpg),
+"""Salam,mən qruplarınızdakı istifadəçiləri tağ etmək üçün köməkçi botam.Məni qrupunuza əlavə edərək insanları rahatlıqla çağıra bilərsiz /help **kömək üçün**[❤](https://telegra.ph/file/2fa3a833f3ccc1d98dba1.jpg),
 """,
     link_preview=False,
     buttons=(
        [
-        Button.url(' support', 'https://t.me/chucky_support'),
-        Button.url('creater', 'https://t.me/kittu_the_criminall')
+        Button.url(' Kanalımız', 'https://t.me/efubotlar'),
+        Button.url('Sahib', 'https://t.me/feridofficaı')
     ],
     )
   )
