fc4dfcce5e13204f2576f6802b70be3a2e8e3c04
feridoffical <110368464+feridoffical@users.noreply.github.com>
2024-05-12 13:50:29 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ async def start(event):
 
 @client.on(events.NewMessage(pattern="^/help$"))
 async def help(event):
-  helptext = "Tağ prosesini başlatmaq üçün:@tag,dayandırmaq üçün isə @cancel. yazmağınız kifayətdir. Nümunə @tag salam"
+  helptext = "Tağ prosesini başlatmaq üçün:/tag, dayandırmaq üçün isə /cancel. yazmağınız kifayətdir. Nümunə @tag salam"
   await event.reply(
     helptext,
     link_preview=False,
@@ -46,7 +46,7 @@ async def help(event):
     )
   )
   
-@client.on(events.NewMessage(pattern="^@all ?(.*)"))
+@client.on(events.NewMessage(pattern="^/tag ?(.*)"))
 async def mentionall(event):
   chat_id = event.chat_id
   if event.is_private:
